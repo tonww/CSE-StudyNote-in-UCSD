cb7242db13fd7385fadbb9265a993bbb3657d470
ShiyuanWen <53202372+ShiyuanWen@users.reply.github.com>
2019-10-20 22:02:04 -0700

    Githubtest
    %s

---
+++ Githubtest.py
@@ -0,0 +1,22 @@
+string = "Hello"
+print("string=%s" % string)
+
+print("string=%5s" % string)
+
+x=1.2345
+
+print("x%s" % x)
+
+print("x%f" % x)
+
+
+print("%+4.2d" % x)
+print("%-4.2f" % x )
+
+print("%09.2f" % x )
+print("%+10.2f" % x )
+print("%-10.2f" % x )
+
+print("%""10.2f" % x )
+
+print("%10.2f" % x )
